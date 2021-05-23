c2427523320209cb86d3fb0d49ebb1d53c48a6c0
jeky22 <qhzh33@gmail.com>
2021-05-24 01:58:21 +0900

    스타일 구현

--- src/components/Feeds.vue
+++ src/components/Feeds.vue
@@ -96,6 +96,3 @@
     },
   };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,6 +2,7 @@
   <div class="modal">
     <div class="modal-card">
       <div>
+        <div class="exit" @click="$emit('close-modal')">X</div>
         <div class="title">필터</div>
         <div class="filter">
           <div
@@ -48,59 +49,3 @@
     },
   };
 </script>
-<style lang="scss">
-  /* Modal */
-  .modal,
-  .overlay {
-    position: fixed;
-    display: flex;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    align-items: center;
-    justify-content: center;
-  }
-  .overlay {
-    opacity: 0.7;
-    background-color: black;
-  }
-  .modal-card {
-    width: 460px;
-    height: 270px;
-    background: white;
-    z-index: 999;
-    padding: 30px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    .title {
-      text-align: left;
-      font: normal normal bold 22px/32px SpoqaHanSans;
-      letter-spacing: 0px;
-      color: #212529;
-      opacity: 1;
-    }
-    .filter {
-      font: normal normal normal 16px/32px SpoqaHanSans;
-      color: #495057;
-      .modal-inner {
-        display: flex;
-        align-items: center;
-      }
-    }
-    button {
-      width: 99px;
-      height: 40px;
-      background: #00c854 0% 0% no-repeat padding-box;
-      border-radius: 3px;
-      border: 0;
-      font: normal normal normal 16px/32px SpoqaHanSans;
-      color: #ffffff;
-    }
-    .flex {
-      display: flex;
-      justify-content: flex-end;
-    }
-  }
-</style>
